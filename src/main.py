f3c8b2fbdbed532493e5ca56a20b5207b35179e1
Har1sh-k <hkolla03@gmail.com>
2025-01-10 14:41:09 -0600

    Email Func updated

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import RSS_getter
+import gmail_sender
 
 NewsLetter_path="src/RSS_Newsletter.txt"
 news,error_stack=RSS_getter.get_NEWS(NewsLetter_path)
 
-for i in news:print(news[i]["Published_date"], news[i]["Link"])
-#print(news)
-print(error_stack)
+gmail_sender.parse_send_news(news)
